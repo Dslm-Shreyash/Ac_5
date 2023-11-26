b97546f5b2e0b4212134d29c0d732b16700515c7
Dslm-HarleyQuinn <captianjacky3@gmail.com>
2023-11-26 17:36:41 +0530

    Idk

--- Encryption_Methods.py
+++ Encryption_Methods.py
@@ -1,8 +1,6 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import rsa
-import hashlib
-
 # key = b'\x10/\xce\xd1\x87BP\x81\x9b\x0e(\xa5\x99g\xd4\x8f'
 key = get_random_bytes(16)
 # key = b"This is the keyy"
